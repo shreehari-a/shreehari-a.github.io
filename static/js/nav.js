4712f19a9b67cccad5d6b7b925e6a8a35a7080a4
shreehari <shreehari.a260649@gmail.com>
2017-05-07 00:40:17 +0530

    Added spider sqlite, skills

---
+++ static/js/nav.js
@@ -0,0 +1,7 @@
+
+$(function(){
+    $('#navigation').html('<div class="btn-group btn-group-justified"><div class="btn-group"><a href="index.html" style="color:white"><button type="button" class="btn btn-primary" style="width:70%;border-radius:0px;float:right;">Home</button></a></div><div class="btn-group"><button type="button" class="btn btn-primary" style="width:70%;border-radius:0px;float:left;">Git Repository</button></div></div>');
+
+});
+
+
