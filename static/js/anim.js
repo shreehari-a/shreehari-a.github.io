e813d1cdcdd2cd5a77c674b73a45b1943433c764
shreehari <shreehari.a260649@gmail.com>
2017-07-20 21:31:04 +0530

    added git repo button

---
+++ static/js/anim.js
@@ -0,0 +1,24 @@
+$(function(){
+$(".animsition").animsition({
+            inClass: 'fade-in-down',
+            outClass: 'fade-out-down',
+            inDuration: 800,
+            outDuration: 800,
+            linkElement: '.animsition-link',
+            // e.g. linkElement: 'a:not([target="_blank"]):not([href^="#"])'
+            loading: true,
+            loadingParentElement: 'body', //animsition wrapper element
+            loadingClass: 'animsition-loading',
+            loadingInner: '', // e.g '<img src="loading.svg" />'
+            timeout: true,
+            timeoutCountdown: 50,
+            onLoadEvent: true,
+            browser: [ 'animation-duration', '-webkit-animation-duration'],
+            // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
+            // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
+            overlay : false,
+            overlayClass : 'animsition-overlay-slide',
+            overlayParentElement : 'body',
+            transition: function(url){ window.location.href = url; }
+});      
+});
